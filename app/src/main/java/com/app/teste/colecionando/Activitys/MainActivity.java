c4f42b35d1ff36749489617679c9d0e6a6c2bcc1
CamillaVeloni <Zz01594120>
2019-03-25 18:27:02 -0300

    Commit Tela Principal - começo

--- app/src/main/java/com/app/teste/colecionando/Activitys/MainActivity.java
+++ app/src/main/java/com/app/teste/colecionando/Activitys/MainActivity.java
@@ -2,6 +2,7 @@ package com.app.teste.colecionando.Activitys;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import com.app.teste.colecionando.R;
 
@@ -11,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Dando a responsabilidade da action bar p/ o toolbar
+        Toolbar toolBar = findViewById(R.id.tool_bar);
+        toolBar.setTitle("Colecionando");
+        // Método para que a toolbar funcione em versões anteriores do android
+        setSupportActionBar(toolBar);
     }
 }
