16ae47122779d75c95d574c0d010c462719bef37
CamillaVeloni <Zz01594120>
2019-03-28 01:21:17 -0300

    Commit NavigationView e Menu

--- app/src/main/java/com/app/teste/colecionando/Activitys/MainActivity.java
+++ app/src/main/java/com/app/teste/colecionando/Activitys/MainActivity.java
@@ -1,7 +1,6 @@
 package com.app.teste.colecionando.Activitys;
 
 import android.os.Bundle;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -11,27 +10,38 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.app.teste.colecionando.ConfiguraçãoFirebase.ConfigFirebase;
 import com.app.teste.colecionando.R;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mAuth = ConfigFirebase.getFirebaseAuth();
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar); // usado para que a toolbar funcione bem com versões anteriores
 
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.addDrawerListener(toggle);
-        toggle.syncState();
+        // CONFIGURAÇÃO DO NAV. DRAWER
+        DrawerLayout drawer = findViewById(R.id.drawer_layout); // Recuperando o layout drawer que fica
+                                                                // no activity_main
 
-        NavigationView navigationView = findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(     // Criação da barra que fica no menu
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        // Passando o contexto, o objeto que recupera a interface, toolbar (onde o toggle é configurado),
+        // os 2 últimos parametros são para pessoas com deficiencias (textos)
+        drawer.addDrawerListener(toggle); // configurar o toggle no drawer
+        toggle.syncState(); // o toggle vai ser sincronizado e carregado novamente
+
+        NavigationView navigationView = findViewById(R.id.nav_view); // Nele é que o usuário pode navegar pelas opções
+        navigationView.setNavigationItemSelectedListener(this); // Método de navegação na própria activity
     }
 
     @Override
@@ -48,7 +58,7 @@ public class MainActivity extends AppCompatActivity
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) { // Implementado por causa do uso da interface
-        // Método chamado sempre que o usuário clicar em uma das opções do NavigationView
+        // Método chamado sempre que o usuário clicar em uma das opções do Nav
         int id = item.getItemId(); // Recuperar o id do item clicado
 
         if (id == R.id.nav_coleções) { // Verificar qual a opção clicada através do id
@@ -70,22 +80,30 @@ public class MainActivity extends AppCompatActivity
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflar o menu
-        getMenuInflater().inflate(R.menu.main, menu);
+        getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Ações para o action bar ficam aqui
-        int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_search) {
-            return true;
-        }else if(id == R.id.action_sair){
-
+        switch (item.getItemId()) {
+            case R.id.action_sair:
+                deslogarUsuario();
+                finish();
+                break;
         }
-
         return super.onOptionsItemSelected(item);
+    }
+
+    public void deslogarUsuario(){
+
+        try {
+            mAuth.signOut();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
     }
 }
